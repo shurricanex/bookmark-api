1bb949e46a37c1cde6311ff0aca6c26930a31eea
sarakorn <sakol.sarakorn@optistech.com>
2022-02-25 17:13:26 +0700

    initial authentication service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,67 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+@Injectable({})
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
+
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      ) {
+        throw new ForbiddenException('Account have been taken');
+      }
+    }
+  }
+
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        email: dto.email,
+      },
+    });
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    if (!pwMatches) throw new ForbiddenException('Credential incorrect');
+
+    delete user.hash;
+    return user;
+  }
+
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: Promise<string> }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const secret = await this.config.get('JWT_SECRET');
+    const signedToken = this.jwt.signAsync(payload, {
+      expiresIn: '15mn',
+      secret,
+    });
+    return { access_token: signedToken };
+  }
+}
